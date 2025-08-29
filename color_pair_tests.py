92321279303ce716bea67d1bb5ed8397d88de452
ArunR-29 <arunvicky1124@gmail.com>
2025-08-29 11:10:59 +0530

    Create color_pair_tests.py

---
+++ color_pair_tests.py
@@ -0,0 +1,20 @@
+from color_pair_utils import color_pair_from_number, pair_number_from_color_pair
+
+def test_number_to_pair(pair_number, expected_major_color, expected_minor_color):
+    """Test conversion from pair number to color pair."""
+    major_color, minor_color = color_pair_from_number(pair_number)
+    assert major_color == expected_major_color
+    assert minor_color == expected_minor_color
+
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
+    """Test conversion from color pair to pair number."""
+    pair_number = pair_number_from_color_pair(major_color, minor_color)
+    assert pair_number == expected_pair_number
+
+if __name__ == '__main__':
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('Done :)')
