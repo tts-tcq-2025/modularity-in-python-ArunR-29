8a30e7916f7cad492a086fdfc1a5ba0b4cdb2c17
ArunR-29 <arunvicky1124@gmail.com>
2025-08-29 11:24:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+"""MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 
@@ -48,4 +48,19 @@ if __name__ == '__main__':
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
-  print('Done :)')
+  print('Done :)')"""
+
+
+from color_pair_tests import test_number_to_pair, test_pair_to_number
+
+def main():
+    # Run test cases for color pair conversions
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('Done :)')
+
+if __name__ == '__main__':
+    main()
